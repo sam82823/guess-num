337bcc37ee0faee9cfadce81a9929d93182a257a
Sam Li <sam82823@gmail.com>
2020-01-21 12:43:59 +0800

    final version

--- r.py
+++ r.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+start = input('number start from: ')
+end = input('number end at: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count +=1 # count = count + 1
