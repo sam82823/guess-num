217e0de895fc2b49a94962f045d70bfa0e83c414
Sam Li <sam82823@gmail.com>
2020-01-21 12:36:48 +0800

    add times

--- r.py
+++ r.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count +=1 # count = count + 1
 	ans = input('guess number: ')
 	ans = int(ans)
 	if ans == r:
 		print('right number!')
+		print('it is the', count, 'time')
 		break
 	elif ans < r:
 		print('the number is bigger')
 	elif ans > r:
 		print('the number is smaller')
+	print('it is the', count, 'time')
